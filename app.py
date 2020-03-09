865a605e40b8924390fce6adce8e9756c8545206
geminerald <zacdwarner@gmail.com>
2020-03-09 10:26:08 +0000

    pushing to heroku

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World Again"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)

c388f02e7584427f7ac7984990fd539732fc9084
geminerald <zacdwarner@gmail.com>
2020-03-09 12:17:47 +0000

    added basic home/ view bugs page and confirmed able to read from DB

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'bugzap'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser1@zwcluster1-znjzd.mongodb.net/bugzap?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World Again"
+@app.route('/get_bugs')
+def get_bugs():
+    return render_template("bugs.html", bugs=mongo.db.bug.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),

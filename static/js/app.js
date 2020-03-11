0427c47c4303265f32d285b037956e80f521b083
geminerald <zacdwarner@gmail.com>
2020-03-11 19:08:07 +0000

    2nd attempt to link to js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
     console.log("page loaded, starting js");
-    $('.collapsible').collapsible();
-    $('select').material_select();
     console.log("page loaded, js complete");
   });

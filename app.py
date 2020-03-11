4c15709ffb3e22089f293fba5fb722742f3024bf
geminerald <zacdwarner@gmail.com>
2020-03-11 11:45:36 +0000

    updated naming convention on new bug page and updated link to image for icon

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_bugs():
 
 @app.route('/new_bug')
 def new_bug():
-    return render_template('newbug.html')
+    return render_template('new_bug.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),

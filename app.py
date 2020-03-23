3d7b8be2777e44a4d7226c54c3ec110fb9dc44dd
geminerald <zacdwarner@gmail.com>
2020-03-23 11:01:08 +0000

    moving buttons to right of collapsible header

--- app.py
+++ app.py
@@ -36,5 +36,14 @@ def edit_bug(bug_id):
     return render_template('editbug.html', bug=the_bug, users=all_users)
 
 
+@app.route('/update_bug/bug_id', methods=["POST"])
+def update_bug(bug_id):
+    bugs=mongo.db.bug
+    bugs.update( {'(_id': ObjectId(bug_id)},
+    {
+           
+    })
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)

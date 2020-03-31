ce336fbae51e42dd3ccc3376d3a2a88d1fdaede0
geminerald <zacdwarner@gmail.com>
2020-03-31 12:22:31 +0000

    added beginning of datestamp

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     console.log("This is the linked page");
+    const startTime = Date.now();
+    console.log("The Start Time Variable from app.js is " + startTime);
   });

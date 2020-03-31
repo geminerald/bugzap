822a6269ffcb19e58ebf555f0eb7fa4f5a482f6e
geminerald <zacdwarner@gmail.com>
2020-03-31 12:48:03 +0000

    moved datestamp to event listener on button

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("This is the linked page");
+
+});
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener('click', () => {
     const currentDate = new Date();
-            const date = currentDate.getDate();
-            const month = currentDate.getMonth();
-            const year = currentDate.getFullYear();
-            const dateString = `Date: ${date}/${month+1}/${year}`
-            console.log(dateString)
-            const timeStamp = document.getElementById("timeStamp");
-            let timeStamp.value = dateString;
-  });
+    const date = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+    const dateString = `Date: ${date}/${month + 1}/${year}`
+    console.log(dateString)
+    const timeStamp = document.getElementById("timeStamp");
+    let timeStamp.value = dateString;
+}

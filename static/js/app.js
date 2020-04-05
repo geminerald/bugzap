733d6ca6f0f204f2327e024f79dc422af66112e2
geminerald <zacdwarner@gmail.com>
2020-04-05 13:39:24 +0000

    altered layout of individual bug desriptions and moved all JS into app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
+
+    //confirmation that page is being read correctly.
     console.log("This is the linked page");
+
+    //Initialise materialize components
+
+    $('.collapsible').collapsible();
+    $('select').material_select();
+    $('.sidenav').sidenav();
+
+    //get date for push to DB on new bug page
     const currentDate = new Date();
     const date = currentDate.getDate();
     const month = currentDate.getMonth();
@@ -7,4 +17,5 @@ $(document).ready(function () {
     const dateString = `${date}/${month + 1}/${year}`
     console.log(dateString);
     document.getElementById("timeStamp").value = dateString;
+
 });
